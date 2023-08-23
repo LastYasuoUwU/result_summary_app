8115a182b1b37944c584350e74cccafd35a56e27
LastYasuoHasagi <lastyasuohasagi@gmail.com>
2023-08-23 13:38:24 +0100

    change data.json location

--- src/app/components/RightSide/index.tsx
+++ src/app/components/RightSide/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import "./styles.scss";
 import StateComponent from "./components/stateComponent";
-import iconreacton from "../../../../public/images/icon-memory.svg";
-// import iconmemory from "../../../assets/images/icon-memory.svg";
-// import iconverbal from "../../../assets/images/icon-verbal.svg";
-// import iconvisual from "../../../assets/images/icon-visual.svg";
 import data from "../../../../data.json";
 
 type Props = {};
